c1eefa86e9700e846bf88e13e708fd36abc75bf0
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-07-26 19:03:15 +0200

    Added Domain project

---
+++ src/Domain/Common/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common;
+
+public abstract class Entity<TId>
+{
+    public TId Id { get; private init; }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+}
---
+++ src/Domain/Common/ValueObject.cs
@@ -0,0 +1,38 @@
+﻿namespace Domain.Common;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || GetType() != obj.GetType())
+            return false;
+
+        var valueObject = (ValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        if (ReferenceEquals(left, null) && ReferenceEquals(right, null))
+            return true;
+
+        if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !(left == right);
+    }
+}
