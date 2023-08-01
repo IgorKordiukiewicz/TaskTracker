c3601d3bb960c96bac93f2e7aad79d8d81b539f5
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-08-01 21:45:59 +0200

    Added User unit tests

---
+++ tests/UnitTests/Domain/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Users;
+
+namespace UnitTests.Domain;
+
+public class UserTests
+{
+    [Fact]
+    public void Create_ShouldCreateUserWithGivenParameters()
+    {
+        var authenticationId = "12345";
+        var name = "UserName";
+
+        var user = User.Create(authenticationId, name);
+
+        using (new AssertionScope())
+        {
+            user.AuthenticationId.Should().Be(authenticationId);
+            user.Name.Should().Be(name);
+            user.Id.Should().NotBeEmpty();
+        }
+    }
+}
