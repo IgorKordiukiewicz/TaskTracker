b19e22f732845bd715c3e10aa7498cc2e51b14de
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-10-14 18:58:28 +0200

    Showing bidirectional transitions as one diagram link

--- src/Web/Client/Diagrams/DiagramFactory.cs
+++ src/Web/Client/Diagrams/DiagramFactory.cs
@@ -5,9 +5,10 @@ namespace Web.Client.Diagrams;
 
 public static class DiagramFactory // TODO: Remove, diagram options have factory parameters?
 {
-    public static LinkModel CreateLink(TaskStatusNodeModel sourceNode, TaskStatusNodeModel targetNode)
+    public static LinkModel CreateLink(TaskStatusNodeModel sourceNode, TaskStatusNodeModel targetNode, bool bidirectional = false)
         => new(sourceNode, targetNode)
         {
             TargetMarker = LinkMarker.Arrow,
+            SourceMarker = bidirectional ? LinkMarker.Arrow : null
         };
 }
