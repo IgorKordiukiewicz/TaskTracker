41b48b28c70c1af9a791b6aaf4583275721c863b
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-10-23 19:50:28 +0200

    Entity now implements IEquatable, etc.

--- src/Domain/Common/Entity.cs
+++ src/Domain/Common/Entity.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Common;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
     public Guid Id { get; private init; }
 
@@ -8,4 +8,19 @@ public abstract class Entity
     {
         Id = id;
     }
+
+    public override bool Equals(object? obj)
+        => obj is Entity entity && Id == entity.Id;
+
+    public bool Equals(Entity? other)
+        => Equals((object?)other);
+
+    public static bool operator ==(Entity left, Entity right) 
+        => Equals(left, right);
+
+    public static bool operator !=(Entity left, Entity right) 
+        => !Equals(left, right);
+
+    public override int GetHashCode()
+        => Id.GetHashCode();
 }
