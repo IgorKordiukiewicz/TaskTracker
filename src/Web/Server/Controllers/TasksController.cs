a7bc08b333d0e648a1ceb9a36ca43ed8e1f91fed
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-01-25 17:08:45 +0100

    Added documentation for tasks controller

--- src/Web/Server/Controllers/TasksController.cs
+++ src/Web/Server/Controllers/TasksController.cs
@@ -2,6 +2,9 @@
 
 namespace Web.Server.Controllers;
 
+/// <summary>
+/// 
+/// </summary>
 [ApiController]
 [Route("tasks")]
 [Authorize]
@@ -9,77 +12,147 @@ public class TasksController : ControllerBase
 {
     private readonly IMediator _mediator;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mediator"></param>
     public TasksController(IMediator mediator)
     {
         _mediator = mediator;
     }
 
+    /// <summary>
+    /// Create a new task
+    /// </summary>
+    /// <remarks>
+    /// Returns ID of the created task.
+    /// </remarks>
+    /// <param name="projectId"></param>
+    /// <param name="model"></param>
+    /// <response code="404">Project not found.</response> 
     [HttpPost]
     [Authorize(Policy.ProjectCreateTasks)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> CreateTask([FromHeader] Guid projectId, [FromBody] CreateTaskDto model)
     {
         var result = await _mediator.Send(new CreateTaskCommand(projectId, model));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Get a list of tasks for a project.
+    /// </summary>
+    /// <param name="projectId"></param>
+    /// <response code="404">Project's workflow not found.</response> 
     [HttpGet]
     [Authorize(Policy.ProjectMember)]
+    [ProducesResponseType(typeof(TasksVM), 200)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> GetTasks([FromHeader] Guid projectId)
     {
         var result = await _mediator.Send(new GetAllTasksQuery(projectId));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Update a task's status.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <param name="model"></param>
+    /// <response code="404">Task or task status not found.</response> 
     [HttpPost("{taskId:guid}/update-status")]
     [Authorize(Policy.ProjectTransitionTasks)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> UpdateTaskStatus(Guid taskId, UpdateTaskStatusDto model)
     {
         var result = await _mediator.Send(new UpdateTaskStatusCommand(taskId, model));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Update a task's priority.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <param name="model"></param>
+    /// <response code="404">Task not found.</response> 
     [HttpPost("{taskId:guid}/update-priority")]
     [Authorize(Policy.ProjectModifyTasks)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> UpdateTaskPriority(Guid taskId, UpdateTaskPriorityDto model)
     {
         var result = await _mediator.Send(new UpdateTaskPriorityCommand(taskId, model));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Update a task's assignee.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <param name="model"></param>
+    /// <response code="404">Task or project member not found.</response> 
     [HttpPost("{taskId:guid}/update-assignee")]
     [Authorize(Policy.ProjectAssignTasks)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> UpdateTaskAssignee(Guid taskId, UpdateTaskAssigneeDto model)
     {
         var result = await _mediator.Send(new UpdateTaskAssigneeCommand(taskId, model));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Update a task's description.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <param name="model"></param>
+    /// <response code="404">Task not found.</response> 
     [HttpPost("{taskId:guid}/update-description")]
     [Authorize(Policy.ProjectAssignTasks)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> UpdateTaskDescription(Guid taskId, UpdateTaskDescriptionDto model)
     {
         var result = await _mediator.Send(new UpdateTaskDescriptionCommand(taskId, model));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Add a new task comment.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <param name="model"></param>
+    /// <response code="404">Task not found.</response> 
     [HttpPost("{taskId:guid}/comments")]
     [Authorize(Policy.ProjectAddComments)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> AddTaskComment(Guid taskId, AddTaskCommentDto model)
     {
         var result = await _mediator.Send(new AddTaskCommentCommand(taskId, User.GetUserAuthenticationId(), model));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Get a list of a task's comments.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <response code="404">Task not found.</response> 
     [HttpGet("{taskId:guid}/comments")]
     [Authorize(Policy.ProjectMember)]
+    [ProducesResponseType(typeof(TaskCommentsVM), 200)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> GetTaskComments(Guid taskId)
     {
         var result = await _mediator.Send(new GetTaskCommentsQuery(taskId));
         return result.ToHttpResult();
     }
 
+    /// <summary>
+    /// Get a list of task's activities.
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <response code="404">Task not found.</response> 
     [HttpGet("{taskId:guid}/activities")]
     [Authorize(Policy.ProjectMember)]
+    [ProducesResponseType(typeof(TaskActivitiesVM), 200)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> GetTaskActivities(Guid taskId)
     {
         var result = await _mediator.Send(new GetTaskActivitiesQuery(taskId));
