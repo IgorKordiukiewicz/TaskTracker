a4d23990cdcbcdd484fd48475c030bf3ac1ec509
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-02-26 00:30:51 +0100

    Added TimeParser

---
+++ src/Web/Client/Common/TimeParser.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Web.Client.Common;
+
+public static class TimeParser
+{
+    public static bool TryToMinutes(string input, out int result)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            result = 0;
+            return false;
+        }
+
+        // Treat integer input as hours ?
+        var isInteger = int.TryParse(input, out var integer);
+        if (isInteger)
+        {
+            result = integer * 60;
+            return true;
+        }
+
+        // match negative numbers too to so method can fail instead of ignoring the group with it
+        const string regex = @"((-*\d+d)? *(-*\d+h)? *(-*\d+m)? *)";
+        var match = Regex.Match(input, regex);
+
+        // fail when match is shorter than input to prevent inputs in wrong order
+        if (!match.Success || match.Value.Length < input.Length)
+        {
+            result = 0;
+            return false;
+        }
+
+        var days = match.Groups[2].Value;
+        var hours = match.Groups[3].Value;
+        var minutes = match.Groups[4].Value;
+        
+        // fail if didn't match any groups or any group has a negative value
+        if((string.IsNullOrEmpty(days) && string.IsNullOrEmpty(hours) && string.IsNullOrEmpty(minutes))
+           || days.StartsWith('-') || hours.StartsWith('-') || minutes.StartsWith('-'))
+        {
+            result = 0;
+            return false;
+        }
+
+        result = (ParseGroup(days) * 24 * 60) + (ParseGroup(hours) * 60) + ParseGroup(minutes);
+        return true;
+    }
+    
+    private static int ParseGroup(string group)
+        => !string.IsNullOrEmpty(group) ? int.Parse(group[..^1]) : 0;
+}
---
+++ tests/UnitTests/Web.Client/TimeParserTests.cs
@@ -0,0 +1,38 @@
+﻿using Web.Client.Common;
+
+namespace UnitTests.Web.Client;
+
+public class TimeParserTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("abc")]
+    [InlineData("5h 3d")] // wrong order
+    [InlineData("1d 15h -5m")] // negative numbers
+    public void TryToMinutes_ShouldFail_WhenInputIsNotCorrect(string input)
+    {
+        var success = TimeParser.TryToMinutes(input, out _);
+
+        success.Should().BeFalse();
+    }
+    
+    [Theory]
+    [InlineData("2", 120)]
+    [InlineData("2d 5h 40m", 3220)]
+    [InlineData("3d", 4320)]
+    [InlineData("4h", 240)]
+    [InlineData("80m", 80)]
+    [InlineData("1d 2h", 1560)]
+    [InlineData("1d 15m", 1455)]
+    [InlineData("8h 100m", 580)]
+    public void TryToMinutes_ShouldSucceedAndParseCorrectly_WhenInputIsCorrect(string input, int expectedMinutes)
+    {
+        var success = TimeParser.TryToMinutes(input, out var result);
+
+        using (new AssertionScope())
+        {
+            success.Should().BeTrue();
+            result.Should().Be(expectedMinutes);
+        }
+    }
+}
