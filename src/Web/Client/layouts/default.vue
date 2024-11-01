093b33cf65081630c6ac3d4db785e4a7d73ec811
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-11-01 20:23:20 +0100

    Click on app name to go to index page

--- src/Web/Client/layouts/default.vue
+++ src/Web/Client/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex">
         <div class="w-80 h-screen border-e-2 p-2 flex flex-col" v-if="sidebarVisible">
-            <p class="text-xl text-center mb-4 font-medium">Task Tracker</p>
+            <p class="text-xl text-center mb-4 font-medium cursor-pointer" @click="navigateTo('/')">Task Tracker</p>
             <NavBar />
         </div>
         <div class="w-full max-h-screen">
