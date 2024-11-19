c2c7ce7365dce06af8b96bdfbc7c496094cf0121
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-11-19 18:48:05 +0100

    Added azure web app (infra)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,3 +18,21 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.baseName}-rg"
   location = var.location
 }
+
+resource "azurerm_service_plan" "serviceplan" {
+    name = "${var.baseName}-serviceplan"
+    location = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    sku_name = "F1"
+    os_type = "Windows"
+}
+
+resource "azurerm_windows_web_app" "api" {
+    name = "${var.baseName}-api"
+    location = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    service_plan_id = azurerm_service_plan.serviceplan.id
+    site_config {
+        always_on = false
+    }
+}
