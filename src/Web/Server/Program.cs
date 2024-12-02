43b8d714a55624b14a51e4c673ca56d6d1b8872c
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-12-02 20:29:16 +0100

    Fixed issue with postgress local datetime

--- src/Web/Server/Program.cs
+++ src/Web/Server/Program.cs
@@ -56,6 +56,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
